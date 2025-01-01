22439bc35d3bfba4eae9d7531275b5571c9c17b0
ShraddhaDarade <shraddhadarade135@gmail.com>
2025-01-01 11:12:01 +0530

    Initial commit for Shivansh project

---
+++ Shivansh.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("/Users/shraddhadarade/Desktop/PROJECTS WINTER BREAK/Student Projects/Shivansh/Shiv.jpeg")
+with col2:
+    st.title("Shivansh Khedkar")
+    content = """
+    Hi! My name is Shivansh, and I’m 5 years old. I love playing with my toys, dancing to my favorite songs, and going on adventures while traveling. Music makes me so happy, and I even sing along sometimes! I love my mom and dad so much—they’re my superheroes and make everything extra fun. Life is amazing, and I enjoy every moment! 😊
+    """
+    st.info(content)
+
+col3, col4 = st.columns(2)
+
+with col3:
+    st.image("/Users/shraddhadarade/Desktop/PROJECTS WINTER BREAK/Student Projects/Shivansh/aaibaba.jpeg")
+
+with col4:
+    st.title("My Mom and Dad")
+    content2="""My Aai is a computer engineer, and she works at IBM. She’s so smart and always helps me learn new things. Aai also cooks the yummiest food for me—I love everything she makes! She gives me the best hugs, and I love her so much. 💖
+                My Baba works at Alfa Laval. He’s super fun, and I love playing with him. We laugh a lot together, and he always makes me feel happy. Baba is so kind and the best dad ever. 💙
+                I’m the luckiest kid to have such awesome parents! 😊"""
+    st.info(content2)
